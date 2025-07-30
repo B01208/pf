3b57fb28ce77aec203aaa36effbd8bf4307f86db
B01208 <tlxag13579111315@gmail.com>
2025-07-30 15:21:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal() {
+  document.getElementById("cvModal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("cvModal").classList.add("hidden");
+}
+
